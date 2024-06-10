eeebff06aab4ef9ce4de7e1eddffb1278dc7292e
Ayslla Gomes <ayslla.gomes@outlook.com>
2024-06-10 20:15:55 -0300

    Atualizando a DTO

--- src/shared/models/end-location.dto.ts
+++ src/shared/models/end-location.dto.ts
@@ -2,20 +2,3 @@ export class EndLocationDto {
   lat: number;
   lng: number;
 }
-
-//distance: { text: '878 km', value: 878085 },
-//   duration: { text: '11 hours 21 mins', value: 40862 },
-//   end_address: 'Conceição da Aparecida, MG, 37148-000, Brazil',
-//   end_location: { lat: -21.094545, lng: -46.2030468 },
-//   start_address: 'Planaltina, State of Goiás, Brazil',
-//   start_location: { lat: -15.4537315, lng: -47.6088769 },
-//   steps: [
-//     {
-//       distance: [Object],
-//       duration: [Object],
-//       end_location: [Object],
-//       html_instructions: 'Head <b>northeast</b> on <b>Av. Elísio Vaz</b>',
-//       polyline: [Object],
-//       start_location: [Object],
-//       travel_mode: 'DRIVING'
-//     },
